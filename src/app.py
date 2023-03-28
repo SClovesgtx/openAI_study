a2b2b58f177101127bf449ba35b1176b1d54acbf
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-03-28 17:26:32 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+import openai
+import gradio as gr
+
+openai.api_key = os.environ.get("API_KEY")
+
+def read_prompt(prompt_name: str) -> str:
+    with open(f"prompts/{prompt_name}.txt") as file:
+        text = file.read()
+    return text
+
+
+def read_messages(prompt_name: str) -> str:
+    with open(f"prompts/{prompt_name}.json") as file:
+        data = json.load(file)
+    return data
+
+
+PROMPT = read_prompt("CEDAE")
+messages = read_messages("CEDAE")
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=messages, 
+            temperature=0, 
+            max_tokens=100
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+inputs = gr.inputs.Textbox(lines=7, label="Chat coma Nina")
+outputs = gr.outputs.Textbox(label="Reply")
+
+gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="Assistente Nina",
+             description="Me pergunte coisas sobre a CEDAE",
+             theme="compact").launch(share=True)
